d42776fbc43299c3ec6eb7f15a13350c0d7c9764
alejgh <alejandrgh11@gmail.com>
2020-07-24 12:50:06 -0400

    Add networkx import and return seed nodes on centrality results

--- herc_common/graph.py
+++ herc_common/graph.py
@@ -31,7 +31,6 @@ def get_centrality_algorithm_results(g, algorithm, stop_uris, top_n):
     """
     metrics = algorithm(g)
     metrics = {key: val for key, val in metrics.items()
-                if g.nodes[key]['n'] != 0
                 and key not in stop_uris}
     best_qids = sorted(metrics, key=metrics.get, reverse=True)[:top_n]
     return [(g.nodes[qid], metrics[qid]) for qid in best_qids]
